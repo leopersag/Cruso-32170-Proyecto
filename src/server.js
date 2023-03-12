311a314241ed21082dbfa220014b943a4c6b98c3
lperalta <leonardo.peralta@mirgor.com.ar>
2023-03-12 19:41:20 -0300

    Proyecto V.11

--- src/server.js
+++ src/server.js
@@ -54,10 +54,17 @@
 
             // Multer
             const multer  = require('multer');
+            const storage = multer.diskStorage ({
+                destination: './public/uploads/',
+                filename: (req, file, done) => {
+                    done (null, Date.now()+'.'+file.mimetype.slice(6))
+                }
+            });
+
             const upload = multer({ 
-                dest: './public/uploads/',
+                storage: storage,
                 fileFilter: (req, file, done) => {
-                    if (file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+                    if (file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
                     return done (null, true);
                     }
                     logger.warn("Invalid type of file")
